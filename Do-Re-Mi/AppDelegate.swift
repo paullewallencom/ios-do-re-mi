68f55b9c0dcdfb1f7dab0370af00893bc63ac6f8
Paul Lewallen <paullewallencom@users.noreply.github.com>
2023-06-10 20:30:23 -0700

    Init Commit
    Init commit.

---
+++ Do-Re-Mi/AppDelegate.swift
@@ -0,0 +1,23 @@
+/*
+ *  AppDelegate.swift
+ *  Do-Re-Mi
+ *
+ *  Created by Paul Lewallen on 6/10/23.
+ *
+ */
+
+import UIKit
+
+// MARK: - AppDelegate: UIResponder, UIApplicationDelegate
+@UIApplicationMain
+class AppDelegate : UIResponder, UIApplicationDelegate {
+    
+    // MARK: Properties
+    var window : UIWindow?
+    
+    // MARK: UIApplicationDelegate
+    func application (_ application : UIApplication, didFinishLaunchingWithOptions launchOptions : [UIApplication.LaunchOptionsKey : Any]? = nil) -> Bool {
+            // Override point for customization after application launch.
+            return true
+        }
+}
---
+++ Do-Re-Mi/ViewController.swift
@@ -0,0 +1,36 @@
+/*
+ *  ViewController.swift
+ *  Do-Re-Mi
+ *
+ *  Created by Paul Lewallen on 6/10/23.
+ *
+ */
+
+import UIKit
+
+// MARK: - ViewController: UIViewController, UITableViewDataSource
+class ViewController : UIViewController, UITableViewDataSource {
+
+    // MARK: Properties
+    // Use this string property as your reuse identifier,
+    // Storyboard has been set up for you using this String.
+    let cellReuseIdentifier = "MyCellReuseIdentifier"
+    
+    // Choose some data to show in your table
+    let model : [String] = [
+        // TODO: Fill this array with data
+    ]
+    
+    // MARK: UITableViewDataSource
+    // Add the two essential table data source methods here
+    func tableView (_ tableView : UITableView, numberOfRowsInSection section : Int) -> Int {
+        //TODO: Implement method to return the correct number of rows.
+        return 0
+    }
+    
+    func tableView (_ tableView : UITableView, cellForRowAt indexPath : IndexPath) -> UITableViewCell {
+        //TODO: Implement method to return cell with the correct reuseidentifier and populated with the correct data.
+        let placeholderCell = UITableViewCell ()
+        return placeholderCell
+    }
+}
